991b6f0c5d86340de33f5abf954749929e041311
kaneken <99122116+kaneken555@users.noreply.github.com>
2024-08-25 10:05:31 +0900

    [del]DB_task

--- myproject/bot/models.py
+++ myproject/bot/models.py
@@ -4,13 +4,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Task(models.Model):
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.description
-
 class Book(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)  # ユーザーと本を関連付けるフィールド
     title = models.CharField(max_length=255)
